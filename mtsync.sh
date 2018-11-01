c7c07aa0776239dcdc6cf0b1f6015a89c818caca
jeb76 <44624863+jeb76@users.noreply.github.com>
2018-10-31 22:24:46 -0300

    Update mtsync.sh

--- mtsync.sh
+++ mtsync.sh
@@ -9,12 +9,13 @@ CONFIGFILE=./mtsync.conf
 
 # Get parameters
 if [ ! -f "$CONFIGFILE" ]
-        then
-                echo -e "\n${LIGHTRED}File $CONFIGFILE does not exist! Please provide the required config file${NC}"
-                echo -e "\nFile $CONFIGFILE does not exist! Please provide the required config file" 2>&1 >>$LOGFILE
-                exit 1
-        else
-                source $CONFIGFILE
+	then
+		MSG="File $CONFIGFILE does not exist! Please provide the required config file"
+		echo -e "\n${LIGHTRED}$MSG${NC}\n"
+		echo -e "\n$MSG\n" 2>&1 >>$LOGFILE
+		exit 1
+	else
+		source $CONFIGFILE
 fi
 
 # Debug
@@ -27,39 +28,41 @@ echo "ADMINPASS=$ADMINPASS"
 echo "FTPPORT=$FTPPORT"
 echo "FTPUSER=$FTPUSER"
 echo "FTPPASS=$FTPPASS"
+echo "EXPORTDIR=$EXPORTDIR"
+echo "IMPORTDIR=$IMPORTDIR"
 echo "TEMPDIR=$TEMPDIR"
 echo "LOGDIR=$LOGDIR"
 echo "LOGFILE=$LOGFILE"
+echo ""
 
-# Check log dir
-if [ ! -d "$LOGDIR" ]
-        then
-                mkdir -p $LOGDIR
-                if [ ! $? -eq 0 ]; then
-                        echo -e "\n${LIGHTRED}Directory $LOGDIR not exists and can not create it!${NC}"
-                        echo -e "\nDirectory $LOGDIR not exists and can not create it!" 2>&1 >> $LOGFILE
-                        exit 1
-                fi
-fi
+# Check dirs
+declare -a DIRARRAY=("$LOGDIR" "$EXPORTDIR" "$IMPORTDIR" "$TEMPDIR")
 
-# Check temp dir
-if [ ! -d "$TEMPDIR" ]
-        then
-                mkdir -p $TEMPDIR
-                if [ ! $? -eq 0 ]; then
-                        echo -e "\n${LIGHTRED}Directory $TEMPDIR not exists and can not create it!${NC}"
-                        echo -e "\nDirectory $TEMPDIR not exists and can not create it!" 2>&1 >> $LOGFILE
-                        exit 1
-                fi
-fi
+for DIR in "${DIRARRAY[@]}"
+do
+	if [ ! -d "$DIR" ]
+		then
+			echo -e "Creating directory $DIR..."
+			mkdir -p $DIR
+			if [ ! $? -eq 0 ]
+				then
+					MSG="Directory $DIR not exists and can not create it!"
+					echo -e "${LIGHTRED}$MSG${NC}"
+					echo -e "$MSG" 2>&1 >> $LOGFILE
+					exit 1
+			fi
+	fi
+done
 
 # Check sshpass
 if (which sshpass> /dev/null 2>&1)
-        then
-                echo -e "\n${LIGHTGREEN}sshpass OK${NC}"
-        else
-                echo -e "\n${LIGHTRED}sshpass is not installed! Please install it to run $0${NC}"
-                echo -e "\nsshpass is not installed! Please install it to run $0" 2>&1 >> $LOGFILE
+	then
+		echo -e "${LIGHTGREEN}sshpass OK${NC}"
+	else
+		MSG="sshpass is not installed! Please install it to run $0"
+		echo -e "${LIGHTRED}$MSG${NC}"
+		echo -e "$MSG" 2>&1 >> $LOGFILE
+		exit 1
 fi
 
 ##### Default mode #####
@@ -69,20 +72,59 @@ fi
 #sshpass -p $ADMINPASS ssh $ADMINUSER@$IPMTMAIN -p $SSHPORT -o StrictHostKeyChecking=no "/export compact file=$EXPORTFILEMTBKP" 2>&1 |tee -a $LOGFILE
 
 # Download export via ftp
-#wget -N -nv -P $TEMPDIR/ ftp://$IPMTMAIN:$FTPPORT/$EXPORTFILEMTMAIN.rsc --ftp-user=$FTPUSER --ftp-password=$FTPPASS 2>&1 |tee -a $LOGFILE
-#wget -N -nv -P $TEMPDIR/ ftp://$IPMTMAIN:$FTPPORT/$EXPORTFILEMTBKP.rsc --ftp-user=$FTPUSER --ftp-password=$FTPPASS 2>&1 |tee -a $LOGFILE
+#wget -N -nv -P $IMPORTDIR/ ftp://$IPMTMAIN:$FTPPORT/$EXPORTFILEMTMAIN.rsc --ftp-user=$FTPUSER --ftp-password=$FTPPASS 2>&1 |tee -a $LOGFILE
+#wget -N -nv -P $IMPORTDIR/ ftp://$IPMTMAIN:$FTPPORT/$EXPORTFILEMTBKP.rsc --ftp-user=$FTPUSER --ftp-password=$FTPPASS 2>&1 |tee -a $LOGFILE
 
 ### Exports comparison
 # Get all modules from exports
-egrep -o '^/.*' $TEMPDIR/$EXPORTFILEMTMAIN.rsc >$TEMPDIR/modules_mtmain.txt
-egrep -o '^/.*' $TEMPDIR/$EXPORTFILEMTBKP.rsc >$TEMPDIR/modules_mtbkp.txt
+if [ -s "$IMPORTDIR/$EXPORTFILEMTMAIN.rsc" ]
+	then
+		egrep -o '^/.*' $IMPORTDIR/$EXPORTFILEMTMAIN.rsc >$TEMPDIR/modules_mtmain.tmp
+	else
+		MSG="File $IMPORTDIR/$EXPORTFILEMTMAIN.rsc not exists or is not readeable! Aborting..."
+		echo -e "${LIGHTRED}$MSG${NC}\n"
+		echo -e "$MSG" 2>&1 >> $LOGFILE
+		exit 2
+fi
+
+if [ -s "$IMPORTDIR/$EXPORTFILEMTBKP.rsc" ]
+	then
+		egrep -o '^/.*' $IMPORTDIR/$EXPORTFILEMTBKP.rsc >$TEMPDIR/modules_mtbkp.tmp
+	else
+		MSG="File $IMPORTDIR/$EXPORTFILEMTBKP.rsc not exists or is not readeable! Aborting..."
+		echo -e "${LIGHTRED}$MSG${NC}\n"
+		echo -e "$MSG" 2>&1 >> $LOGFILE
+		exit 2
+fi
 
 # Modules only in mt bkp
-grep -Fxvf $TEMPDIR/modules_mtmain.txt $TEMPDIR/modules_mtbkp.txt >$TEMPDIR/modules_mtbkp_only.txt
+grep -Fxvf $TEMPDIR/modules_mtmain.tmp $TEMPDIR/modules_mtbkp.tmp >$TEMPDIR/modules_mtbkp_only.tmp
 
-# for MODULE in $TEMPDIR/modules_mtbkp_only.txt
-# do
-# sed to insert remove [find] under module name
+# For every module only in mt bkp create an export file to remove it
+if [ -s "$TEMPDIR/modules_mtbkp_only.tmp" ]
+	then
+		while read LINE; do
+			echo "$LINE" |sed -e 's/^\/.*$/&\nremove \[find default=no\]/g' >>$EXPORTDIR/remove.auto.rsc
+		done <$TEMPDIR/modules_mtbkp_only.tmp
+fi
+
+# For every module (Block of export script) of mt main compare with every module of mt main and generate an export file
+# with differences
+
+## Solucionar problema con sdiff
+if [ -s "$TEMPDIR/modules_mtmain.tmp" ]
+	then
+		while read MODULE; do
+			MODULEMTMAIN=$(echo $MODULE |sed -e 's/\///g' -e 's/\s/_/g' -e 's/$/_mtmain.rsc/g')
+			MODULEMTBKP=$(echo $MODULE |sed -e 's/\///g' -e 's/\s/_/g' -e 's/$/_mtbkp.rsc/g')
+			MODULEEXPORT=$(echo $MODULE |sed -e 's/\///g' -e 's/\s/_/g' -e 's/$/.auto.rsc/g')
+			awk 'f && /^\//{exit} /^\'"$MODULE"'/{f=1} f' $IMPORTDIR/$EXPORTFILEMTMAIN.rsc >$TEMPDIR/$MODULEMTMAIN
+			awk 'f && /^\//{exit} /^\'"$MODULE"'/{f=1} f' $IMPORTDIR/$EXPORTFILEMTBKP.rsc >$TEMPDIR/$MODULEMTBKP
+			yes "1" |sdiff -sa $TEMPDIR/$MODULEMTMAIN $TEMPDIR/$MODULEMTBKP -o $MODULEEXPORT
+			echo ""
+			sleep 2s
+		done <$TEMPDIR/modules_mtmain.tmp
+fi
 
 #awk "f && /^\//{exit} /^\/interface bridge/{f=1} f" $EXPORTFILEMTMAIN.rsc
 
